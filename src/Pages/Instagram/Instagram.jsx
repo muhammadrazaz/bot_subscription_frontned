c90353d40ba4e45752abaa677e7c44f681f7e7e7
Muhammad Mudassir <mudassirm734@gmail.com>
2024-10-13 21:24:53 +0500

    with edit and delete button small size

--- src/Pages/Instagram/Instagram.jsx
+++ src/Pages/Instagram/Instagram.jsx
@@ -733,7 +733,7 @@ export default function Instagram() {
                                 {postWaitListData.map((data, index) => {
                                     return <div className='post-item'>
                                         {
-                                            true && <>
+                                            data.img && <>
                                                 <img className='post-img' src={axios.defaults.baseURL.split('/a')[0]+data.img} alt="" />
                                                 <div className='post-action'>
                                                     <button className='m-1 p-1' onClick={(e) => { editClick(data) }}>
