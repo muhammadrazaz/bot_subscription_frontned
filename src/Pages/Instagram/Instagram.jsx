314d03a60a5206095119488216ce5535054e1579
Muhammad Mudassir <mudassirm734@gmail.com>
2024-10-13 11:18:32 +0500

    with wait and posted font change

--- src/Pages/Instagram/Instagram.jsx
+++ src/Pages/Instagram/Instagram.jsx
@@ -73,7 +73,7 @@ export default function Instagram() {
 
     const formatTime = (seconds) => {
         const hrs = Math.floor(seconds / 3600);
-        const mins = Math.floor((seconds % 3600) / 60);
+        const mins = Math.floor((seconds % 3600) / 60).toString().padStart(2, '0');
         // const secs = seconds % 60;
         return `${hrs}:${mins} h`;
     };
